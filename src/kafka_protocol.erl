82fa22498444c8fe20373386b6776951994d1300
David Almroth <david@campanja.com>
2013-04-08 16:34:33 +0200

    now with protocol v0.8 support, the server accepts messages, but nothing is visible in consumer yet

--- src/kafka_protocol.erl
+++ src/kafka_protocol.erl
@@ -7,7 +7,6 @@
 -export([producer_request/2]).
 
 
--define(RQ_TYPE_PRODUCE, 0).
 -define(DEFAULT_PARTION, 0).
 -define(DEFAULT_MAGIC, 1).
 -define(DEFAULT_COMPRESSION, 0).
@@ -28,17 +27,13 @@ producer_request(Topic,  Messages) ->
 %%%-------------------------------------------------------------------
 
 -spec producer_request(Topic::binary(),
-      	              Partition::integer(),
-		      Magic::integer(),
- 		      Compression::integer(),
-		      Messages::list()) -> binary().
+                       Partition::integer(),
+                       Magic::integer(),
+                       Compression::integer(),
+                       Messages::list()) -> binary().
 producer_request(Topic, Partition, Magic, Compression, Messages) ->
-   MessagesLength = size_of_produce_messages(Messages),
-   %%io:format("Messages_Length = ~w~n", [MessagesLength]),
-   TopicSize = size(Topic),
-   RequestSize = 2 + 2 + TopicSize + 4 + 4 + MessagesLength,
-
-   ProducedMessages =
+    %%io:format("Messages_Length = ~w~n", [MessagesLength]),
+    ProducedMessages =
         lists:foldr(
           fun (Payload, Acc) ->
                   KafkaMessage = create_message(Payload, Magic, Compression),
@@ -46,31 +41,61 @@ producer_request(Topic, Partition, Magic, Compression, Messages) ->
           end,
           <<"">>,
           Messages),
-    <<RequestSize:32/integer,
-      ?RQ_TYPE_PRODUCE:16/integer,
-      TopicSize:16/integer,
-      Topic/binary,
-      Partition:32/integer,
-      MessagesLength:32/integer,
-      ProducedMessages/binary >>.
+    MessagesLength = size(ProducedMessages),
+    TopicSize = size(Topic),
+    ClientID = <<"client1">>,
+    CLientIdSize = size(ClientID),
+    RequestSize = 2 + 2 + 4 + 2 + CLientIdSize +
+        2 + 4 + 4+ 2 + TopicSize +
+        4 + 4 + 4 + MessagesLength,
+
+    <<
+      %% 1 + 4 ful-fält
+      RequestSize:32/signed-integer,
+
+      %% This is the headder for one request message
+      0:16/signed-integer, % API key for preduce request = 0
+      0:16/signed-integer, % API version
+      0:32/signed-integer, % CorreleationID
+      CLientIdSize:16/signed-integer, % ClientID size of string
+      ClientID/binary,        % ClientID
+
+      %% This is the produceRequest = one request message
+      0:16/signed-integer, % RequiredAccs
+      0:32/signed-integer, % Timeout
+      1:32/signed-integer, % 1 hardcoded "topic array"
+      TopicSize:16/signed-integer, %Size of the Topic string
+      Topic/binary,                % Topic string
+      1:32/signed-integer, % hardcoded 1 "partition+messageset array"
+      Partition:32/signed-integer,
+      MessagesLength:32/signed-integer,
+      ProducedMessages/binary
+    >>.
 
 
 
 create_message(Payload, Magic, Compression) ->
-    MessageLength = 1+1+4+size(Payload),
-    CheckSum = erlang:crc32(Payload),
-    <<MessageLength:32/integer,
-      Magic:8/integer,
-      Compression:8/integer,
-      CheckSum:32/integer,
-      Payload/binary>>.
+    PayloadSize = size(Payload),
+    KeyBytes = <<"">>, %hardcoded empty key
+    KeyByteSize = size(KeyBytes),
 
-size_of_produce_messages(Messages) ->
-    lists:foldl(fun (X, AccSize) ->
-                        size(X) + AccSize + 4 + 1 + 1 + 4
-                end,
-                0,
-                Messages).
+    MessageBinary =
+        <<
+          Magic:8/signed-integer,
+          Compression:8/signed-integer, % Attributes = Compression ?
+          KeyByteSize:32/signed-integer, %length of the Key byte array
+          KeyBytes/binary,
+          PayloadSize:32/signed-integer, %length of the Value byte array
+          Payload/binary>>,
+
+    CheckSum = erlang:crc32(MessageBinary),
+    MessageLength = 8 + 4 + 4 + size(MessageBinary),
+    <<0:64/signed-integer, %Offset = 0
+      MessageLength:32/signed-integer,
+      CheckSum:32/integer,
+      MessageBinary/binary>>.
+
+
 
 
 
